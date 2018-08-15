1b6b4b414ce7a10622df01676313c83479530aa2
anghel7 <ghehahanghel@gmail.com>
2018-08-15 19:31:57 -0400

    Generando componente producto-item en modulo producto

---
+++ src/app/producto/producto-item/producto-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-producto-item',
+  templateUrl: './producto-item.component.html',
+  styleUrls: ['./producto-item.component.css']
+})
+export class ProductoItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/producto/producto.module.ts
+++ src/app/producto/producto.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductoItemComponent } from './producto-item/producto-item.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [ProductoItemComponent]
 })
 export class ProductoModule { }
