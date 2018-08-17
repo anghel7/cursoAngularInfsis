fe7cfd2e26747b19206b604072c5ec56b13c5939
anghel7 <ghehahanghel@gmail.com>
2018-08-17 19:42:39 -0400

    Definicio de un modelo para producto y uso de string interpolation

---
+++ src/app/producto/models/producto.interface.ts
@@ -0,0 +1,5 @@
+export interface Producto {  
+  nombre: string,
+  precio: number,
+  stock: number
+}
--- src/app/producto/producto-item/producto-item.component.ts
+++ src/app/producto/producto-item/producto-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Producto } from '../models/producto.interface';
 
 @Component({
   selector: 'app-producto-item',
@@ -7,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductoItemComponent implements OnInit {
 
-  constructor() { }
+  producto: Producto;
+  titulo: string = 'Producto';
+  // alt + shift + f windows
+  // ctrl + shift + i linux
+  constructor() {
+    this.producto = {
+      nombre: "Coca Colla",
+      precio: 12.5,
+      stock: 10
+    };
+  }
 
   ngOnInit() {
   }
