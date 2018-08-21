91ac4d7d45fe8ed754a02cc799f4eed4efec8f3f
anghel7 <ghehahanghel@gmail.com>
2018-08-20 20:15:18 -0400

    Directivas structurales

--- src/app/producto/producto-list/producto-list.component.ts
+++ src/app/producto/producto-list/producto-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Producto } from '../models/producto.interface';
 
 @Component({
   selector: 'app-producto-list',
@@ -7,18 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductoListComponent implements OnInit {
 
-  titulo:string;
-  campoBusqueda:string="hola";
+  titulo: string;
+  campoBusqueda: string = "hola";
+
+  listaProductos: Producto[];
 
   constructor() {
+    this.listaProductos = [];
     this.titulo = "Lista de productos"
   }
 
   ngOnInit() {
+    this.listaProductos = [
+      {
+        nombre: "Detergent",
+        precio: 5,
+        stock: 2,
+        cantidad: 10,
+        marca: "Patido",
+        id: 1
+      },
+      {
+        nombre: "Aceite",
+        precio: 10,
+        stock: 5,
+        cantidad: 40,
+        marca: ""
+      },
+      {
+        nombre: "Fosforo",
+        precio: 2,
+        stock: 7,
+        cantidad: 15,
+        marca: "Acme"
+      }
+    ];
   }
 
-  buscar():void{
-    this.campoBusqueda="nuevo busqueda";
+  buscar(): void {
+    this.campoBusqueda = "nuevo busqueda";
   }
 
 }
