c707c765264fcaefa2d197a5332b2f187f4539e0
anghel7 <ghehahanghel@gmail.com>
2018-08-21 19:55:25 -0400

    Configurando el modulo material

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {  } from '@angular/material';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserAnimationsModule
   ],
+  exports:[],
   declarations: []
 })
 export class MaterialModule { }
