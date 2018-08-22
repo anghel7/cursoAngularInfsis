ada88cc8d6c04c202c5c17a8a8321f6d96c5d40b
anghel7 <ghehahanghel@gmail.com>
2018-08-22 19:37:58 -0400

    Creando rutas para el modulo appRouting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { ProductoListComponent } from './producto/producto-list/producto-list.component';
+import { HomeComponent } from './layout/home/home.component';
+
+const routesApp: Routes = [
+  {
+    path: 'producto/lista',
+    component: ProductoListComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routesApp)
   ],
+  exports: [RouterModule],
   declarations: []
 })
 export class AppRoutingModule { }
