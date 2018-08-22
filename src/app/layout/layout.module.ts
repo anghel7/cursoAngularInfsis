25a35d4d1fe29f44244b1c3ae2b7ab47d34e4759
anghel7 <ghehahanghel@gmail.com>
2018-08-22 19:12:28 -0400

    Generando component footer en el modulo layout

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [HeaderComponent, HomeComponent]
+  declarations: [HeaderComponent, HomeComponent, FooterComponent]
 })
 export class LayoutModule { }
