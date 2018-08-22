d895c8adbcffc08c992b45afda717270d5b27605
anghel7 <ghehahanghel@gmail.com>
2018-08-22 19:09:55 -0400

    Generando component home en el modulo layout

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [HeaderComponent, HomeComponent]
 })
 export class LayoutModule { }
