30f69b9cf89296c589bf02611cafad42445e1159
anghel7 <ghehahanghel@gmail.com>
2018-08-23 19:15:30 -0400

    Creando rutas para crear y editar un producto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,21 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductoListComponent } from './producto/producto-list/producto-list.component';
 import { HomeComponent } from './layout/home/home.component';
+import { ProductoCreateComponent } from './producto/producto-create/producto-create.component';
+import { ProductoEditComponent } from './producto/producto-edit/producto-edit.component';
 
 const routesApp: Routes = [
   {
     path: 'producto/lista',
     component: ProductoListComponent
+  },
+  {
+    path:'producto/crear',
+    component: ProductoCreateComponent
+  },
+  {
+    path:'producto/editar',
+    component: ProductoEditComponent
   },
   {
     path: 'home',
