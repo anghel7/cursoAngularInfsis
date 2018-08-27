6fbf3297251d216b682ff7f57637e55c68db2473
anghel7 <ghehahanghel@gmail.com>
2018-08-27 19:09:55 -0400

    Creando firmas para el servicio producto

--- src/app/producto/services/producto.service.ts
+++ src/app/producto/services/producto.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Producto } from '../models/producto.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductoService {
 
+  //get, getall, create, update, delete
   constructor() { }
+
+  getAllProductos(): Producto[] {
+    return [];
+  }
+
+  getProductoById(id: number): Producto {
+    return null;
+  }
+
+  createProducto(productoToCreate: Producto): Producto {
+    return null;
+  }
+
+  updateProducto(id: number, productoToUpdate: Producto): Producto {
+    return null;
+  }
+
+  deleteProductoById(id: number): Producto {
+    return null;
+  }
+
 }
